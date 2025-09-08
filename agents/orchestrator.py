776e556e229b56b4bd5b7cd7b7f767f3f8aa38bd
thomas bush <thomasbush52@gmail.com>
2025-09-08 17:52:54 +0200

    orchestrator paper finding from keys

--- agents/orchestrator.py
+++ agents/orchestrator.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from tqdm import tqdm
+from agents.utils import build_pubmed_query_from_keywords
 from dotenv import load_dotenv
 from typing import List, Dict, Any, Optional, Literal
 from argparse import ArgumentParser
@@ -9,6 +10,7 @@ import dspy
 import pandas as pd
 
 from agents.keyword_exp import KeywordGeneratorProgram, SynonymGeneratorProgram
+from agents.paper_finder import fetch_from_keywords, articles_fetchers, append_filters
 
 
 
@@ -35,9 +37,15 @@ if __name__ == "__main__":
     keywords = keyword_gen(query)
     boolean_keys = keywords["boolean_pubmed"]
     keywords = keywords["keywords"]
+
     # skip syn now
     print(f"keywords Generated:{keywords}")
+    q = build_pubmed_query_from_keywords(keywords, field="tiab", mesh_hints=None)
+    q = append_filters(q, english=True, humans=True, year_from=2015)
+    print(q)
 
+#    df = articles_fetchers(q, n=n, include_citations=False)
+#    print(df.head())
 
 
 
